002fcb27430f9ffcecf597cbe7107ddaa7f6a8f5
ActuallyPanda56 <nietoc0595@gmail.com>
2024-09-27 23:03:10 -0500

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
-  API_URL: process.env.VERCEL_URL ?? "http://localhost:3000",
+  API_URL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000",
   PORT: process.env.NEXT_PUBLIC_PORT ?? "3000",
 };
